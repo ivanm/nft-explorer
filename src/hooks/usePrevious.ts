7fb88600be8b2748ba741ad7cd82716f96d8e5e8
Ivan Mayoral <iimayoral@gmail.com>
2021-11-10 20:01:05 -0600

    Updates

--- src/hooks/usePrevious.ts
+++
@@ -1,9 +0,0 @@
-import { useEffect, useRef } from "react";
-const usePrevious = (value: any) => {
-  const ref = useRef();
-  useEffect(() => {
-    ref.current = value;
-  });
-  return ref.current;
-};
-export default usePrevious;
